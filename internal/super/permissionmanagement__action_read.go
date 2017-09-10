ce9cbc9a1efa5c29782807065bd1c4fc6559df10
Jörg Pernfuß <code.jpe@gmail.com>
2017-09-10 22:39:45 +0000

    super: update action handler

---
+++ internal/super/permissionmanagement__action_read.go
@@ -0,0 +1,141 @@
+/*-
+ * Copyright (c) 2016-2017, Jörg Pernfuß
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package super // import "github.com/mjolnir42/soma/internal/super"
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/mjolnir42/soma/internal/msg"
+	"github.com/mjolnir42/soma/lib/proto"
+)
+
+func (s *Supervisor) actionRead(q *msg.Request, mr *msg.Result) {
+	switch q.Action {
+	case msg.ActionList:
+		s.actionList(q, mr)
+	case msg.ActionShow:
+		s.actionShow(q, mr)
+	case msg.ActionSearch:
+		s.actionSearch(q, mr)
+	}
+}
+
+func (s *Supervisor) actionList(q *msg.Request, mr *msg.Result) {
+	var (
+		err                             error
+		rows                            *sql.Rows
+		actionID, actionName, sectionID string
+	)
+
+	if rows, err = s.stmtActionList.Query(); err != nil {
+		mr.ServerError(err, q.Section)
+		return
+	}
+
+	for rows.Next() {
+		if err = rows.Scan(
+			&actionID,
+			&actionName,
+			&sectionID,
+		); err != nil {
+			rows.Close()
+			mr.ServerError(err, q.Section)
+			return
+		}
+		mr.ActionObj = append(mr.ActionObj, proto.Action{
+			Id:        actionID,
+			Name:      actionName,
+			SectionId: sectionID,
+		})
+	}
+	if err = rows.Err(); err != nil {
+		mr.ServerError(err, q.Section)
+		return
+	}
+	mr.OK()
+}
+
+func (s *Supervisor) actionShow(q *msg.Request, mr *msg.Result) {
+	var (
+		err                             error
+		ts                              time.Time
+		actionID, actionName, sectionID string
+		category, user, sectionName     string
+	)
+
+	if err = s.stmtActionShow.QueryRow(
+		q.ActionObj.Id,
+	).Scan(
+		&actionID,
+		&actionName,
+		&sectionID,
+		&sectionName,
+		&category,
+		&user,
+		&ts,
+	); err == sql.ErrNoRows {
+		mr.NotFound(err, q.Section)
+		return
+	} else if err != nil {
+		mr.ServerError(err, q.Section)
+		return
+	}
+	mr.ActionObj = append(mr.ActionObj, proto.Action{
+		Id:          actionID,
+		Name:        actionName,
+		SectionId:   sectionID,
+		SectionName: sectionName,
+		Category:    category,
+		Details: &proto.DetailsCreation{
+			CreatedBy: user,
+			CreatedAt: ts.Format(msg.RFC3339Milli),
+		},
+	})
+	mr.OK()
+}
+
+func (s *Supervisor) actionSearch(q *msg.Request, mr *msg.Result) {
+	var (
+		err                             error
+		rows                            *sql.Rows
+		actionID, actionName, sectionID string
+	)
+
+	if rows, err = s.stmtActionSearch.Query(
+		q.ActionObj.Name,
+		q.ActionObj.SectionId,
+	); err != nil {
+		mr.ServerError(err, q.Section)
+		return
+	}
+
+	for rows.Next() {
+		if err = rows.Scan(
+			&actionID,
+			&actionName,
+			&sectionID,
+		); err != nil {
+			rows.Close()
+			mr.ServerError(err, q.Section)
+			return
+		}
+		mr.ActionObj = append(mr.ActionObj, proto.Action{
+			Id:        actionID,
+			Name:      actionName,
+			SectionId: sectionID,
+		})
+	}
+	if err = rows.Err(); err != nil {
+		mr.ServerError(err, q.Section)
+		return
+	}
+	mr.OK()
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
---
+++ internal/super/permissionmanagement__action_write.go
@@ -0,0 +1,130 @@
+/*-
+ * Copyright (c) 2016-2017, Jörg Pernfuß
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package super // import "github.com/mjolnir42/soma/internal/super"
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/mjolnir42/soma/internal/msg"
+	"github.com/mjolnir42/soma/internal/stmt"
+	uuid "github.com/satori/go.uuid"
+)
+
+func (s *Supervisor) actionWrite(q *msg.Request, mr *msg.Result) {
+	if s.readonly {
+		mr.BadRequest(
+			fmt.Errorf(`Readonly instance`),
+			q.Section,
+		)
+		return
+	}
+
+	switch q.Action {
+	case msg.ActionAdd:
+		s.actionAdd(q, mr)
+	case msg.ActionRemove:
+		s.actionRemove(q, mr)
+	}
+
+	if mr.IsOK() {
+		go func() {
+			s.Update <- msg.CacheUpdateFromRequest(q)
+		}()
+	}
+}
+
+func (s *Supervisor) actionAdd(q *msg.Request, mr *msg.Result) {
+	var (
+		err error
+		res sql.Result
+	)
+
+	q.ActionObj.Id = uuid.NewV4().String()
+	if res, err = s.stmtActionAdd.Exec(
+		q.ActionObj.Id,
+		q.ActionObj.Name,
+		q.ActionObj.SectionId,
+		q.AuthUser,
+	); err != nil {
+		mr.ServerError(err, q.Section)
+		return
+	}
+	if mr.RowCnt(res.RowsAffected()) {
+		mr.ActionObj = append(mr.ActionObj, q.ActionObj)
+	}
+}
+
+func (s *Supervisor) actionRemove(q *msg.Request, mr *msg.Result) {
+	var (
+		err error
+		tx  *sql.Tx
+		res sql.Result
+	)
+	txMap := map[string]*sql.Stmt{}
+
+	// open multi-statement transaction
+	if tx, err = s.conn.Begin(); err != nil {
+		mr.ServerError(err, q.Section)
+		return
+	}
+
+	// prepare statements for this transaction
+	for name, statement := range map[string]string{
+		`action_tx_remove`:    stmt.ActionRemove,
+		`action_tx_removeMap`: stmt.ActionRemoveFromMap,
+	} {
+		if txMap[name], err = tx.Prepare(statement); err != nil {
+			err = fmt.Errorf("s.ActionTx.Prepare(%s) error: %s",
+				name, err.Error())
+			mr.ServerError(err, q.Section)
+			tx.Rollback()
+			return
+		}
+	}
+
+	if res, err = s.actionRemoveTx(q.ActionObj.Id,
+		txMap); err != nil {
+		mr.ServerError(err, q.Section)
+		tx.Rollback()
+		return
+	}
+
+	// sets r.OK()
+	if !mr.RowCnt(res.RowsAffected()) {
+		tx.Rollback()
+		return
+	}
+
+	// close transaction
+	if err = tx.Commit(); err != nil {
+		mr.ServerError(err, q.Section)
+		return
+	}
+
+	mr.ActionObj = append(mr.ActionObj, q.ActionObj)
+}
+
+func (s *Supervisor) actionRemoveTx(id string,
+	txMap map[string]*sql.Stmt) (sql.Result, error) {
+	var (
+		err error
+		res sql.Result
+	)
+
+	// remove action from all permissions
+	if res, err = txMap[`action_tx_removeMap`].Exec(
+		id); err != nil {
+		return res, err
+	}
+
+	// remove action
+	return txMap[`action_tx_remove`].Exec(id)
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
