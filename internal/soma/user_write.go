d95d084ea70cba76e80bb9c217c089b60ec28a67
Jörg Pernfuß <code.jpe@gmail.com>
2017-09-13 22:30:18 +0000

    soma: use correct sync keywords

--- internal/soma/user_write.go
+++ internal/soma/user_write.go
@@ -83,11 +83,11 @@ func (w *UserWrite) process(q *msg.Request) {
 	msgRequest(w.reqLog, q)
 
 	switch q.Action {
-	case msg.ActionCreate:
+	case msg.ActionAdd:
 		w.add(q, &result)
 	case msg.ActionUpdate:
 		w.update(q, &result)
-	case msg.ActionDelete:
+	case msg.ActionRemove:
 		w.remove(q, &result)
 	case msg.ActionPurge:
 		w.purge(q, &result)
