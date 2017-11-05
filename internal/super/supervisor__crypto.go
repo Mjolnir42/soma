0a49ef7f35100a81c7662b05a27e38f3f58dafcf
Jörg Pernfuß <code.jpe@gmail.com>
2017-11-05 17:13:22 +0000

    super: merge kexInit() into crypto file

--- internal/super/supervisor__crypto.go
+++ internal/super/supervisor__crypto.go
@@ -94,4 +94,76 @@ func (s *Supervisor) encrypt(kex *auth.Kex, token *auth.Token, mr *msg.Result) e
 	return nil
 }
 
+// kexInit handles key exchange requests
+func (s *Supervisor) kexInit(q *msg.Request) {
+	result := msg.FromRequest(q)
+	result.Super.Verdict = 401
+	q.Log(s.reqLog)
+
+	kex := q.Super.Kex
+	var err error
+	var attemptIV int
+
+	// kexInit is a master instance function
+	if s.readonly {
+		result.ReadOnly()
+		goto dispatch
+	}
+
+	// generate new initialization vector
+	err = kex.GenerateNewVector()
+	for err != nil {
+		attemptIV++
+		if attemptIV > 5 {
+			result.ServerError(err, q.Section)
+			goto dispatch
+		}
+		err = kex.GenerateNewVector()
+	}
+
+	// check no bad IV was generated, with bad being defined
+	// as likely to use 0 as counter
+	err = s.checkIV(kex.InitializationVector)
+	for err != nil {
+		attemptIV++
+		if attemptIV > 5 {
+			result.ServerError(err, q.Section)
+			goto dispatch
+		}
+		err = s.checkIV(kex.InitializationVector)
+	}
+
+	// record the kex submission time
+	kex.SetTimeUTC()
+
+	// record the client ip address
+	kex.SetIPAddressExtractedString(q.RemoteAddr)
+
+	// generate a request ID
+	kex.GenerateNewRequestID()
+
+	// set the client submitted public key as peer key
+	kex.SetPeerKey(kex.PublicKey())
+
+	// generate our own keypair
+	kex.GenerateNewKeypair()
+
+	// save kex
+	s.kex.insert(kex)
+
+	// send out reply
+	result.Super = msg.Supervisor{
+		Verdict: 200,
+		Kex: auth.Kex{
+			Public:               kex.Public,
+			InitializationVector: kex.InitializationVector,
+			Request:              kex.Request,
+		},
+	}
+	result.OK()
+
+dispatch:
+	q.Reply <- result
+}
+
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
