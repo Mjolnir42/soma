581369ecb702fefe6aec82d16271ee57a334a4ad
Jörg Pernfuß <code.jpe@gmail.com>
2018-06-11 06:46:38 +0000

    msg: Add msg.New() and required helpers

--- internal/msg/request.go
+++ internal/msg/request.go
@@ -9,7 +9,11 @@
 package msg
 
 import (
+	"net/http"
+	"strings"
+
 	"github.com/Sirupsen/logrus"
+	"github.com/julienschmidt/httprouter"
 	"github.com/mjolnir42/soma/lib/proto"
 	uuid "github.com/satori/go.uuid"
 )
@@ -107,6 +111,48 @@ func (r *Request) Log(l *logrus.Logger) {
 		r.AuthUser,
 		r.RemoteAddr,
 	)
+}
+
+// New returns a new request
+func New(r *http.Request, params httprouter.Params) Request {
+	returnChannel := make(chan Result, 1)
+	return Request{
+		ID:         requestID(params),
+		RemoteAddr: remoteAddr(r),
+		AuthUser:   authUser(params),
+		Reply:      returnChannel,
+	}
+}
+
+// requestID extracts the RequestID set by Basic Authentication, making
+// the ID consistent between all logs
+func requestID(params httprouter.Params) (id uuid.UUID) {
+	id, _ = uuid.FromString(params.ByName(`RequestID`))
+	return
+}
+
+// authUser returns the extracted authenticated user
+func authUser(params httprouter.Params) string {
+	return params.ByName(`AuthenticatedUser`)
+}
+
+// remoteAddr extracts the IP address part of the IP:port string
+// set as net/http.Request.RemoteAddr. It handles IPv4 cases like
+// 192.0.2.1:48467 and IPv6 cases like [2001:db8::1%lo0]:48467
+func remoteAddr(r *http.Request) string {
+	var addr string
+
+	switch {
+	case strings.Contains(r.RemoteAddr, `]`):
+		// IPv6 address [2001:db8::1%lo0]:48467
+		addr = strings.Split(r.RemoteAddr, `]`)[0]
+		addr = strings.Split(addr, `%`)[0]
+		addr = strings.TrimLeft(addr, `[`)
+	default:
+		// IPv4 address 192.0.2.1:48467
+		addr = strings.Split(r.RemoteAddr, `:`)[0]
+	}
+	return addr
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
