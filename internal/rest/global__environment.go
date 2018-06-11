fa690064d79f02d9c48c64093c37f5d901bb839c
Jörg Pernfuß <code.jpe@gmail.com>
2018-06-11 08:33:08 +0000

    rest: import Environment endpoint from cmd/soma

---
+++ internal/rest/global__environment.go
@@ -0,0 +1,146 @@
+/*-
+ * Copyright (c) 2016, 1&1 Internet SE
+ * Copyright (c) 2015-2018, Jörg Pernfuß
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package rest // import "github.com/mjolnir42/soma/internal/rest"
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/mjolnir42/soma/internal/msg"
+	"github.com/mjolnir42/soma/lib/proto"
+)
+
+// EnvironmentList function
+func (x *Rest) EnvironmentList(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	defer panicCatcher(w)
+
+	request := msg.New(r, params)
+	request.Section = msg.SectionEnvironment
+	request.Action = msg.ActionList
+
+	if !x.isAuthorized(&request) {
+		dispatchForbidden(&w, nil)
+		return
+	}
+
+	handler := x.handlerMap.Get(`environment_r`)
+	handler.Intake() <- request
+	result := <-request.Reply
+	sendMsgResult(&w, &result)
+}
+
+// EnvironmentShow function
+func (x *Rest) EnvironmentShow(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	defer panicCatcher(w)
+
+	request := msg.New(r, params)
+	request.Section = msg.SectionEnvironment
+	request.Action = msg.ActionShow
+	request.Environment = proto.Environment{
+		Name: params.ByName(`environment`),
+	}
+
+	if !x.isAuthorized(&request) {
+		dispatchForbidden(&w, nil)
+		return
+	}
+
+	handler := x.handlerMap.Get(`environment_r`)
+	handler.Intake() <- request
+	result := <-request.Reply
+	sendMsgResult(&w, &result)
+}
+
+// EnvironmentAdd function
+func (x *Rest) EnvironmentAdd(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	defer panicCatcher(w)
+
+	cReq := proto.NewEnvironmentRequest()
+	if err := decodeJSONBody(r, &cReq); err != nil {
+		dispatchBadRequest(&w, err)
+		return
+	}
+	request := msg.New(r, params)
+	request.Section = msg.SectionEnvironment
+	request.Action = msg.ActionAdd
+	request.Environment = proto.Environment{
+		Name: cReq.Environment.Name,
+	}
+
+	if !x.isAuthorized(&request) {
+		dispatchForbidden(&w, nil)
+		return
+	}
+
+	handler := x.handlerMap.Get(`environment_w`)
+	handler.Intake() <- request
+	result := <-request.Reply
+	sendMsgResult(&w, &result)
+}
+
+// EnvironmentRemove function
+func (x *Rest) EnvironmentRemove(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	defer panicCatcher(w)
+
+	request := msg.New(r, params)
+	request.Section = msg.SectionEnvironment
+	request.Action = msg.ActionRemove
+	request.Environment = proto.Environment{
+		Name: params.ByName(`environment`),
+	}
+
+	if !x.isAuthorized(&request) {
+		dispatchForbidden(&w, nil)
+		return
+	}
+
+	handler := x.handlerMap.Get(`environment_w`)
+	handler.Intake() <- request
+	result := <-request.Reply
+	sendMsgResult(&w, &result)
+}
+
+// EnvironmentRename function
+func (x *Rest) EnvironmentRename(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	defer panicCatcher(w)
+
+	cReq := proto.NewEnvironmentRequest()
+	if err := decodeJSONBody(r, &cReq); err != nil {
+		dispatchBadRequest(&w, err)
+		return
+	}
+	request := msg.New(r, params)
+	request.Section = msg.SectionEnvironment
+	request.Action = msg.ActionRename
+	request.Environment = proto.Environment{
+		Name: cReq.Environment.Name,
+	}
+	request.Update = msg.UpdateData{
+		Environment: proto.Environment{
+			Name: cReq.Environment.Name,
+		},
+	}
+
+	if !x.isAuthorized(&request) {
+		dispatchForbidden(&w, nil)
+		return
+	}
+
+	handler := x.handlerMap.Get(`environment_w`)
+	handler.Intake() <- request
+	result := <-request.Reply
+	sendMsgResult(&w, &result)
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
