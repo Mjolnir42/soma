f2ed86e2885833f2ebe4fe5c8feda0d3c12daeea
Jörg Pernfuß <code.jpe@gmail.com>
2018-06-11 11:52:33 +0000

    soma: update keywords for AttributeWrite

--- internal/soma/attribute_write.go
+++ internal/soma/attribute_write.go
@@ -81,10 +81,10 @@ func (w *AttributeWrite) process(q *msg.Request) {
 	msgRequest(w.reqLog, q)
 
 	switch q.Action {
-	case msg.ActionCreate:
-		w.create(q, &result)
-	case msg.ActionDelete:
-		w.delete(q, &result)
+	case msg.ActionAdd:
+		w.add(q, &result)
+	case msg.ActionRemove:
+		w.remove(q, &result)
 	default:
 		result.UnknownRequest(q)
 	}
@@ -92,8 +92,8 @@ func (w *AttributeWrite) process(q *msg.Request) {
 	q.Reply <- result
 }
 
-// create inserts a new attribute
-func (w *AttributeWrite) create(q *msg.Request, mr *msg.Result) {
+// add inserts a new attribute
+func (w *AttributeWrite) add(q *msg.Request, mr *msg.Result) {
 	var (
 		err error
 		res sql.Result
@@ -111,8 +111,8 @@ func (w *AttributeWrite) create(q *msg.Request, mr *msg.Result) {
 	}
 }
 
-// delete removes an attribute
-func (w *AttributeWrite) delete(q *msg.Request, mr *msg.Result) {
+// remove deletes an attribute
+func (w *AttributeWrite) remove(q *msg.Request, mr *msg.Result) {
 	var (
 		err error
 		res sql.Result
