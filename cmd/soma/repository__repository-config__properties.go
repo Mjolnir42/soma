07fa659ef6c1bcd509c36feace8364d4e13799d3
Pernfuß, Jörg <joerg.pernfuss@ionos.com>
2019-01-18 10:56:36 +0100

    cmd/soma: reuse variousPropertyDestroy for repository-config

---
+++ cmd/soma/repository__repository-config__properties.go
@@ -0,0 +1,69 @@
+/*-
+ * Copyright (c) 2015-2018, Jörg Pernfuß
+ * Copyright (c) 2018, 1&1 IONOS SE
+ * Copyright (c) 2016, 1&1 Internet SE
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package main // import "github.com/mjolnir42/soma/cmd/soma"
+
+import (
+	"github.com/codegangsta/cli"
+	"github.com/mjolnir42/soma/lib/proto"
+)
+
+// repositoryConfigPropertyCreateSystem function
+// soma repository property create system ${system} on ${repository} view ${view} \
+//      value ${value} [inheritance ${inherit}] [childrenonly ${child}]
+func repositoryConfigPropertyCreateSystem(c *cli.Context) error {
+	return variousPropertyCreate(c, proto.PropertyTypeSystem, proto.EntityRepository)
+}
+
+// repositoryConfigPropertyCreateCustom function
+// soma repository property create custom ${custom} on ${repository} view ${view} \
+//      value ${value} [inheritance ${inherit}] [childrenonly ${child}]
+func repositoryConfigPropertyCreateCustom(c *cli.Context) error {
+	return variousPropertyCreate(c, proto.PropertyTypeCustom, proto.EntityRepository)
+}
+
+// repositoryConfigPropertyCreateService function
+// soma repository property create service ${service} on ${repository} view ${view}
+//      [inheritance ${inherit}] [childrenonly ${child}]
+func repositoryConfigPropertyCreateService(c *cli.Context) error {
+	return variousPropertyCreate(c, proto.PropertyTypeService, proto.EntityRepository)
+}
+
+// repositoryConfigPropertyCreateOncall function
+// soma repository property create oncall ${oncall} on ${repository} view ${view}
+//      [inheritance ${inherit}] [childrenonly ${child}]
+func repositoryConfigPropertyCreateOncall(c *cli.Context) error {
+	return variousPropertyCreate(c, proto.PropertyTypeOncall, proto.EntityRepository)
+}
+
+// repositoryConfigPropertyDestroySystem function
+// soma repository property destroy system ${system} on ${repository} view ${view}
+func repositoryConfigPropertyDestroySystem(c *cli.Context) error {
+	return variousPropertyDestroy(c, proto.PropertyTypeSystem, proto.EntityRepository)
+}
+
+// repositoryConfigPropertyDestroyCustom function
+// soma repository property destroy custom ${custom} on ${repository} view ${view}
+func repositoryConfigPropertyDestroyCustom(c *cli.Context) error {
+	return variousPropertyDestroy(c, proto.PropertyTypeCustom, proto.EntityRepository)
+}
+
+// repositoryConfigPropertyDestroyService function
+// soma repository property destroy service ${service} on ${repository} view ${view}
+func repositoryConfigPropertyDestroyService(c *cli.Context) error {
+	return variousPropertyDestroy(c, proto.PropertyTypeService, proto.EntityRepository)
+}
+
+// repositoryConfigPropertyDestroyOncall function
+// soma repository property destroy oncall ${oncall} on ${repository} view ${view}
+func repositoryConfigPropertyDestroyOncall(c *cli.Context) error {
+	return variousPropertyDestroy(c, proto.PropertyTypeOncall, proto.EntityRepository)
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
---
+++ cmd/soma/repository__various__property-destroy.go
@@ -0,0 +1,131 @@
+/*-
+ * Copyright (c) 2015-2019, Jörg Pernfuß
+ * Copyright (c) 2019, 1&1 IONOS SE
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package main // import "github.com/mjolnir42/soma/cmd/soma"
+
+import (
+	"fmt"
+
+	"github.com/codegangsta/cli"
+	"github.com/mjolnir42/soma/internal/adm"
+	"github.com/mjolnir42/soma/lib/proto"
+)
+
+// variousPropertyDestroy is the generic function for destroying
+// properties on tree objects
+func variousPropertyDestroy(c *cli.Context, propertyType, entity string) error {
+	switch entity {
+	case proto.EntityRepository, proto.EntityBucket, proto.EntityGroup, proto.EntityCluster, proto.EntityNode:
+	default:
+		return fmt.Errorf("Unknown entity: %s", entity)
+	}
+	switch propertyType {
+	case proto.PropertyTypeSystem, proto.PropertyTypeCustom, proto.PropertyTypeService, proto.PropertyTypeOncall:
+	case proto.PropertyTypeNative:
+		return fmt.Errorf(`Native properties are for introspection and can not be created on tree objects`)
+	case proto.PropertyTypeTemplate:
+		return fmt.Errorf(`Template properties can not be created on tree objects`)
+	default:
+		return fmt.Errorf("Unknown property type: %s", propertyType)
+	}
+
+	opts := map[string][]string{}
+	multipleAllowed := []string{}
+	uniqueOptions := []string{`on`, `view`}
+	mandatoryOptions := []string{`on`, `view`}
+
+	switch entity {
+	case proto.EntityGroup, proto.EntityCluster:
+		uniqueOptions = append(uniqueOptions, `in`)
+		mandatoryOptions = append(mandatoryOptions, `in`)
+	}
+
+	if err := adm.ParseVariadicArguments(
+		opts,
+		multipleAllowed,
+		uniqueOptions,
+		mandatoryOptions,
+		c.Args().Tail(),
+	); err != nil {
+		return err
+	}
+
+	var (
+		repositoryID, bucketID, teamID, serviceID string
+		property, sourceID, path, command         string
+		err                                       error
+	)
+
+	// id lookup
+	switch entity {
+	case proto.EntityRepository:
+		if repositoryID, err = adm.LookupRepoID(opts[`on`][0]); err != nil {
+			return err
+		}
+		if err = adm.LookupTeamByRepo(repositoryID, &teamID); err != nil {
+			return err
+		}
+	case proto.EntityBucket:
+		if bucketID, err = adm.LookupBucketID(opts[`on`][0]); err != nil {
+			return err
+		}
+		if teamID, err = adm.LookupTeamByBucket(bucketID); err != nil {
+			return err
+		}
+		if repositoryID, err = adm.LookupRepoByBucket(bucketID); err != nil {
+			return err
+		}
+	case proto.EntityGroup, proto.EntityCluster, proto.EntityNode:
+	default:
+		return fmt.Errorf("Unknown entity: %s", entity)
+	}
+
+	switch propertyType {
+	case proto.PropertyTypeService:
+		if serviceID, err = adm.LookupServicePropertyID(c.Args().First(), teamID); err != nil {
+			return err
+		}
+		property = serviceID
+	case proto.PropertyTypeSystem:
+		if err := adm.ValidateSystemProperty(
+			c.Args().First()); err != nil {
+			return err
+		}
+		property = c.Args().First()
+	default:
+		property = c.Args().First()
+	}
+
+	switch entity {
+	case proto.EntityRepository:
+		if err = adm.FindRepoPropSrcID(propertyType, property,
+			opts[`view`][0], repositoryID, &sourceID); err != nil {
+			return err
+		}
+		path = fmt.Sprintf("/repository/%s/property/%s/%s",
+			repositoryID, propertyType, sourceID,
+		)
+		command = `repository-config::property-destroy`
+	case proto.EntityBucket:
+		if err = adm.FindBucketPropSrcID(propertyType, property,
+			opts[`view`][0], bucketID, &sourceID); err != nil {
+			return err
+		}
+		path = fmt.Sprintf("/repository/%s/bucket/%s/property/%s/%s",
+			repositoryID, bucketID, propertyType, sourceID,
+		)
+		command = `bucket-config::property-destroy`
+	case proto.EntityGroup, proto.EntityCluster, proto.EntityNode:
+	default:
+		return fmt.Errorf("Unknown entity: %s", entity)
+	}
+
+	return adm.Perform(`delete`, path, command, nil, c)
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
