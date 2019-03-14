5b6f3fe4aebe221ece7c3aa62b1711a1655573a5
Jörg Pernfuß <code.jpe@gmail.com>
2019-03-14 10:55:28 +0000

    stmt: update AuthorizedBucketSearch

--- internal/stmt/bucket::search.go
+++ internal/stmt/bucket::search.go
@@ -39,7 +39,8 @@ JOIN        inventory.user AS creator
 WHERE       inventory.user.uid = $3::varchar
   AND       soma.authorizations_global.category = 'omnipotence'
   AND       soma.permission.name = 'omnipotence'
-  AND       $1::varchar = 'bucket'
+  AND       (   $1::varchar = 'bucket'
+             OR $1::varchar = 'bucket-config' )
   AND       $2::varchar = 'search'
   AND       (soma.buckets.bucket_id      = $4::uuid    OR $4::uuid    IS NULL)
   AND       (soma.buckets.bucket_name    = $5::varchar OR $5::varchar IS NULL)
@@ -74,7 +75,8 @@ WHERE       auth.admin.uid = $3::varchar
   AND       soma.authorizations_global.category = 'system'
   AND       soma.section.name = $1::varchar
   AND       soma.action.name  = $2::varchar
-  AND       $1::varchar = 'bucket'
+  AND       (   $1::varchar = 'bucket'
+             OR $1::varchar = 'bucket-config' )
   AND       $2::varchar = 'search'
   AND       (soma.buckets.bucket_id      = $4::uuid    OR $4::uuid    IS NULL)
   AND       (soma.buckets.bucket_name    = $5::varchar OR $5::varchar IS NULL)
@@ -104,7 +106,8 @@ JOIN        soma.action
   ON        soma.section.id = soma.action.section_id
             -- grant must be scoped on target repository
 JOIN        soma.buckets
-  ON        soma.authorizations_repository.repository_id = soma.buckets.repository_id
+  ON        (   soma.authorizations_repository.repository_id = soma.buckets.repository_id
+             OR soma.authorizations_repository.bucket_id     = soma.buckets.bucket_id    )
 JOIN        inventory.user AS creator
   ON        soma.buckets.created_by = creator.id
 WHERE       inventory.user.uid = $3::varchar
@@ -112,7 +115,8 @@ WHERE       inventory.user.uid = $3::varchar
   AND NOT   inventory.user.is_deleted
   AND       soma.section.name = $1::varchar
   AND       soma.action.name  = $2::varchar
-  AND       $1::varchar = 'bucket'
+  AND       (   $1::varchar = 'bucket'
+             OR $1::varchar = 'bucket-config' )
   AND       $2::varchar = 'search'
             -- section grant for all actions has soma.permission_map.action_id as NULL
   AND       (   soma.permission_map.action_id = soma.action.id
@@ -121,7 +125,54 @@ WHERE       inventory.user.uid = $3::varchar
   AND       (soma.buckets.bucket_name    = $5::varchar OR $5::varchar IS NULL)
   AND       (soma.buckets.repository_id  = $6::uuid    OR $6::uuid    IS NULL)
   AND       (soma.buckets.environment    = $7::varchar OR $7::varchar IS NULL)
-  AND       (soma.buckets.bucket_deleted = $8::boolean OR $8::boolean IS NULL);`
+  AND       (soma.buckets.bucket_deleted = $8::boolean OR $8::boolean IS NULL)
+UNION
+-- CASE 4:  regular user has team scoped bucket::search, which allows to find
+--          buckets owned by that team
+SELECT      soma.buckets.bucket_id,
+            soma.buckets.bucket_name,
+            soma.buckets.repository_id,
+            creator.uid,
+            soma.buckets.created_at,
+            soma.buckets.environment,
+            soma.buckets.bucket_deleted
+FROM        inventory.user
+JOIN        soma.authorizations_team
+            -- authorization could be on the user or inherited from the team
+  ON        (   inventory.user.id      = soma.authorizations_team.user_id
+             OR inventory.user.team_id = soma.authorizations_team.team_id)
+JOIN        soma.permission_map
+  ON        soma.authorizations_team.permission_id = soma.permission_map.permission_id
+JOIN        soma.section
+  ON        soma.permission_map.section_id = soma.section.id
+JOIN        soma.action
+  ON        soma.section.id = soma.action.section_id
+            -- grant must be scoped on target bucket owner team
+JOIN        soma.buckets
+  ON        soma.authorizations_team.authorized_team_id = soma.buckets.organizational_team_id
+JOIN        soma.repository
+  ON        soma.buckets.repository_id = soma.repository.id
+JOIN        inventory.user AS creator
+  ON        soma.buckets.created_by = creator.id
+WHERE       inventory.user.uid = $3::varchar
+  AND       inventory.user.is_active
+  AND NOT   inventory.user.is_deleted
+  AND       soma.section.name = $1::varchar
+  AND       soma.action.name  = $2::varchar
+  AND       (   $1::varchar = 'bucket'
+             OR $1::varchar = 'bucket-config' )
+  AND       $2::varchar = 'search'
+            -- section grant for all actions has soma.permission_map.action_id as NULL
+  AND       (   soma.permission_map.action_id = soma.action.id
+             OR soma.permission_map.action_id IS NULL                 )
+  AND       (soma.buckets.bucket_id      = $4::uuid    OR $4::uuid    IS NULL)
+  AND       (soma.buckets.bucket_name    = $5::varchar OR $5::varchar IS NULL)
+  AND       (soma.buckets.repository_id  = $6::uuid    OR $6::uuid    IS NULL)
+  AND       (soma.buckets.environment    = $7::varchar OR $7::varchar IS NULL)
+  AND       (soma.buckets.bucket_deleted = $8::boolean OR $8::boolean IS NULL)
+            -- only find buckets from deleted repositories if the search is
+            -- for deleted buckets
+  AND       (soma.repository.is_deleted = 'no'::boolean OR $8::boolean = 'yes'::boolean);`
 
 func init() {
 	m[AuthorizedBucketSearch] = `AuthorizedBucketSearch`
